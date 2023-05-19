976b991260d4b680096482f30ae8e31c48f80146
Colm Doyle <collydoyle_88@hotmail.com>
2023-05-19 16:04:55 +0000

    base & index html templates set up

--- punchlist/asgi.py
+++ punchlist/asgi.py
@@ -8,6 +8,16 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+"""
+ASGI config for punchlist project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
 
 from django.core.asgi import get_asgi_application
 
